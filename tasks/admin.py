3bf2cd46f73729b6c8c1b5a482d2f7bfd874fbc9
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-10-29 08:29:42 +0545

    feat: Created db to store multiple tasks of user

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'is_completed', 'created_at', 'updated_at')
+    list_filter = ('title','user','is_completed', 'created_at')
+    search_fields = ('title', 'description')
