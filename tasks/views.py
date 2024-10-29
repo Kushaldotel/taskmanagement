0e9cbaffbb3baf0219c2b3362f8dacfd4f2e7008
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-10-29 13:02:16 +0545

    feat: User can reterive update and delete task associated with them

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.exceptions import PermissionDenied
 
 class TaskListCreateView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
@@ -14,3 +15,24 @@ class TaskListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         # Set the logged-in user as the task owner
         serializer.save(user=self.request.user)
+
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Ensure users can only reterive their own tasks
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_update(self, serializer):
+        # Ensure that only the owner of the task can update it
+        if self.get_object().user != self.request.user:
+            raise PermissionDenied("You do not have permission to edit this task.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        # Ensure that only the owner of the task can delete it
+        if instance.user != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this task.")
+        instance.delete()
